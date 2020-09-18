9ef483cb5a71902435770ed91f06c60025ddedd7
Kipmevrouw <70896873+Kipmevrouw@users.noreply.github.com>
2020-09-18 14:48:45 +0200

    Add files via upload

---
+++ Ditbenik.py
@@ -0,0 +1,55 @@
+#Doel: Programma in python waar iets te voorschijn
+
+print("Hello you!, ik ben Becky")
+print("Wie ben jij?")
+naam = input("Typ je naam in:")
+
+print("Hello " + naam )
+
+print("Ik ben een nieuwkomer op het Mediacollege Amsterdam")
+print("Door een aantal vragen over mij te beandwoorden leer je mij beter kennen.")
+
+
+
+print("Voor ik naar het MBO op het Mediacollege Amsterdam kwam, zat ik op het")
+print("A. Noorderlicht")
+print("B. Had ik geen school")
+print("C. Clusiuscollege")
+antwoord = input("Kies A, B of C >>>>")
+if(antwoord == "A"):
+	print("Correct!")
+elif(antwoord == "B"):
+	print("Fout!")
+elif(antwoord == "C"):
+	print("Fout!")
+ 
+print("Hoe oud ben ik?")
+print("A. 50")
+print("B. 36")
+print("C. 16")
+antwoord = input("Kies A, B of C >>>>")
+if(antwoord == "A"):
+	print("Fout!")
+elif(antwoord == "B"):
+	print("Fout!")
+elif(antwoord == "C"):
+	print("Correct!")
+
+print("Heb ik huisdieren?")
+print("A. Ja, een hond")
+print("B. Ja, een hond en een hamster")
+print("C. Ja, een kat")
+antwoord = input("Kies A, B of C >>>>")
+if(antwoord == "A"):
+	print("Fout!")
+elif(antwoord == "B"):
+	print("Correct!")
+elif(antwoord == "C"):
+	print("Fout!")
+
+print("Bedankt voor het beantwoorden van de vragen.")
+
+
+
+
+
